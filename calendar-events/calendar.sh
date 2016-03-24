f93de6b9c9602a0b00a899a1f522eccddc583334
david.genesis.cruz <david.genesis.cruz@bizreach.co.jp>
2016-03-24 14:56:17 +0900

    Added weather forecast to calendar-events

--- calendar-events/calendar.sh
+++ calendar-events/calendar.sh
@@ -31,6 +31,10 @@ tomorrowTimes=($(/usr/local/bin/icalBuddy -ec "特定日サービス" -eep "atte
 IFS=$'|'
 todayLocations=($(/usr/local/bin/icalBuddy -ec "特定日サービス" -eep "attendees" -n -nc -b "|" -iep "title,location" eventsToday))
 tomorrowLocations=($(/usr/local/bin/icalBuddy -ec "特定日サービス" -eep "attendees" -nc -n -b '|' -iep "title,location" eventsFrom:tomorrow to:tomorrow))
+weatherUrl="http://xml.weather.yahoo.com/forecastrss?p=JAXX0085&u=c"
+weatherForecastLow=$(curl -s "$weatherUrl" | grep "forecast" | head -n5 | tail -n1 | cut -d'"' -f6)
+weatherForecastHigh=$(curl -s "$weatherUrl" | grep "forecast" | head -n5 | tail -n1 | cut -d'"' -f8)
+weatherForecastCondition=$(curl -s "$weatherUrl" | grep "forecast" | head -n5 | tail -n1 | cut -d'"' -f10)
 
 #---------- 表示 ------------#
 echo
@@ -40,16 +44,25 @@ echo " ==========================="
 for i in ${!todayTitles[@]}
 do
 		location=$(echo ${todayLocations[$((i+1))]} | grep "location: " | sed -e "s/.*location: //")
-		if [ -n "$location" ]
-			then echo " ${todayTimes[$i]} ＠ $location"
-			else echo " ${todayTimes[$i]}"
+		if [ "${todayTimes[$i]}" != "today" ]
+		then
+			if [ -n "$location" ]
+				then echo " ${todayTimes[$i]} ＠ $location"
+				else echo " ${todayTimes[$i]}"
+			fi
+			echo "　${todayTitles[$i]}"
+		else # All day events
+			if [ -n "$location" ]
+				then echo " 《《 ${todayTitles[$i]} @ $location 》》"
+				else echo " 《《 ${todayTitles[$i]} 》》"
+			fi 
 		fi
-		echo "　${todayTitles[$i]}"
 		echo
 done
 
 echo
 echo " TOMORROW"
+echo " $weatherForecastCondition ($weatherForecastLow ~ $weatherForecastHigh ℃)"
 echo " ==========================="
 
 tomorrowCnt=${#todayTitles[@]}
@@ -57,11 +70,19 @@ while (( $tomorrowCnt < ${#tomorrowTitles[@]} ))
 do
 		time=${tomorrowTimes[$tomorrowCnt]/tomorrow at /}
 		location=$(echo ${tomorrowLocations[$((tomorrowCnt+1))]} | grep "location: " | sed -e "s/.*location: //")
-		if [ -n "$location" ]
-			then echo " $time ＠ $location"
-			else echo " $time"
+		if [ "$time" != "tomorrow" ]
+		then
+			if [ -n "$location" ]
+				then echo " $time ＠ $location"
+				else echo " $time"
+			fi
+			echo "　${tomorrowTitles[$tomorrowCnt]}"
+		else # All day events
+			if [ -n "$location" ]
+				then echo " 《《 ${tomorrowTitles[$tomorrowCnt]} @ $location 》》"
+				else echo " 《《 ${tomorrowTitles[$tomorrowCnt]} 》》"
+			fi
 		fi
-		echo "　${tomorrowTitles[$tomorrowCnt]}"
 		echo
 		((tomorrowCnt++))
 done
